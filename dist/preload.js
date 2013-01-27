ff2f216963a16bb7cf7a2ce4c30abd008b567b3b
David Wee <rook2pawn@gmail.com>
2013-01-27 02:24:33 -0800

    dist for js script include

---
+++ dist/preload.js
@@ -0,0 +1,55 @@
+;(function (exports) {
+    var ImageSet = function(params) {
+        if (params === undefined) 
+            params = {}
+        var list = params.obj || [];
+        var success = params.fn || undefined;
+        var error = params.fn2 || undefined;
+        var count = 0;
+        if (params.Image !== undefined)
+            Image = params.Image;
+        var myimages = [];
+        this.add = function(src) {
+            list.push(src);
+            return this
+        }
+        this.success = function(fn) {
+            success = fn;
+            return this
+        }
+        this.error = function(fn) {
+            error = fn;
+            return this
+        }
+        this.loaded = function() {
+            count++;
+            if (count === list.length) {
+                success(myimages);
+            }
+        };
+        this.done = function() {
+            if (success !== undefined)
+                list.forEach(function(src) {
+                    var that = this;
+                    var img = new Image();
+                    img.onerror = function() {
+                        if (error !== undefined) error("image load error!");
+                    };
+                    img.onabort = function() {
+                        if (error !== undefined) error("image load abort!");
+                    };
+                    img.onload = function() {
+                        that.loaded();
+                    };
+                    img.src = src;
+                    img.name = src.slice(src.lastIndexOf('/')+1);
+                    myimages.push(img);
+                },this);
+        };
+    };
+    if (exports.Window !== undefined) {
+        exports.Preloader = ImageSet;
+    } else if ((module !== undefined) && (module.exports !== undefined)) {
+        exports = module.exports = ImageSet;
+    }
+})(typeof exports === 'undefined' ?  this : exports)
